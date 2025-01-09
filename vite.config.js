ab48cb71ba4804530fb9dbaca5e52c0b8388cd12
chamce <chancellor.whitaker@eku.edu>
2025-01-09 14:41:43 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// const outDir = "Y:/Reports/DataPage2";
+
+// const base = "";
+
+const base = "/static";
+
+const outDir = base.substring(1);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { outDir },
+  base,
 });
