a5454ba7311fc5b1afaf1e527cd13286c3d12448
chamce <chancellor.whitaker@eku.edu>
2025-01-09 16:10:01 -0500

    toggle labels

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,33 @@ import Remote from "./Remote";
 export default function App() {
   const [key, setKey] = useState(0);
 
+  const [labelsDisplayed, setLabelsDisplayed] = useState(true);
+
+  const toggleLabels = () => setLabelsDisplayed((x) => !x);
+
   const iterateKey = () => setKey((x) => x + 1);
 
   return (
     <Remote
       toolbar={
-        <button className="btn btn-primary" onClick={iterateKey} type="button">
-          Reset map
-        </button>
+        <div className="d-flex flex-wrap gap-2">
+          <button
+            className="btn btn-primary"
+            onClick={iterateKey}
+            type="button"
+          >
+            Reset map
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={toggleLabels}
+            type="button"
+          >
+            {labelsDisplayed ? "Hide" : "Show"} labels
+          </button>
+        </div>
       }
+      className={labelsDisplayed ? "" : "hide-leaflet-tooltips"}
       heading="Current Enrollment Heatmap"
     >
       <Heatmap key={key}></Heatmap>
